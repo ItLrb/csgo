e8dc4906004bc23f235be20d272cd76dfa2cba14
Italo <134540029+ItLrb@users.noreply.github.com>
2023-08-31 22:35:09 -0300

    dasdsa

---
+++ play.js
@@ -0,0 +1,7 @@
+function exibirDiv() {
+    const div = document.getElementById("exit");
+    div.style.display = "block";
+} function ocultarDiv() {
+    const div = document.getElementById("exit");
+    div.style.display = "none";
+}
